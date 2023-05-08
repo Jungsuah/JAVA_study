87374cedad642921fb29d0caa37e79a847ec167b
jungsuah <sua971109@naver.com>
2023-05-08 23:44:33 +0900

    0508 문제코드수정 commit

--- daily0424/src/daily0424/Solution11.java
+++ daily0424/src/daily0424/Solution11.java
@@ -1,36 +1,51 @@
 package daily0424;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 
-
-
-public class Solution11 {//회장선거
-	public String solution(String[] votes, int k){
+class Solution11 {
+	public String solution(String[] votes, int k) {// 회장선거
 		String answer = " ";
-		HashMap<String, String> name = new HashMap<>();
-		List<String> candidate = new ArrayList<>();
-		for (String string : votes) {
-			String[] arr = string.split(" ");
-			System.out.println(Arrays.toString(arr));
-			name.put(arr[0], arr[1]);
-			candidate.add(arr[1]);
+		HashMap<String, HashSet<String>> voteHash = new HashMap<>();
+		HashMap<String, Integer> candidate = new HashMap<>();
+		HashMap<String, Integer> present = new HashMap<>();
+		for (String x : votes) {
+			String a = x.split(" ")[0];
+			String b = x.split(" ")[1];
+			voteHash.putIfAbsent(a, new HashSet<String>());
+			voteHash.get(a).add(b);
+			candidate.put(b, candidate.getOrDefault(b, 0) + 1);
 		}
-		
-		System.out.println(candidate);
-	
-		
-		
+		int max = Integer.MIN_VALUE;
+		for (String a : voteHash.keySet()) {
+			int cnt = 0;
+			for (String b : voteHash.get(a)) {
+				if (candidate.get(b) >= k)
+					cnt++;
+			}
+			present.put(a, cnt);
+			max = Math.max(max, cnt);
+		}
+		ArrayList<String> tmp = new ArrayList<>();
+		for (String name : present.keySet()) {
+			if (present.get(name) == max)
+				tmp.add(name);
+		}
+		tmp.sort((a, b) -> a.compareTo(b));
+		answer = tmp.get(0);
 		return answer;
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Solution11 T = new Solution11();
-		System.out.println(T.solution(new String[]{"john tom", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john"}, 2));
-//		System.out.println(T.solution(new String[]{"john tom", "park luis", "john luis", "luis tom", "park tom", "luis john", "luis park", "park john", "john park", "tom john", "tom park", "tom luis"}, 2));
-//		System.out.println(T.solution(new String[]{"cody tom", "john tom", "cody luis", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john"}, 2));
-//		System.out.println(T.solution(new String[]{"bob tom", "bob park", "park bob", "luis park", "daniel luis", "luis bob", "park luis", "tom bob", "tom luis", "john park", "park john"}, 3));
+		System.out.println(T.solution(
+				new String[] { "john tom", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john" }, 2));
+		System.out.println(T.solution(new String[] { "john tom", "park luis", "john luis", "luis tom", "park tom",
+				"luis john", "luis park", "park john", "john park", "tom john", "tom park", "tom luis" }, 2));
+		System.out.println(T.solution(new String[] { "cody tom", "john tom", "cody luis", "daniel luis", "john luis",
+				"luis tom", "daniel tom", "luis john" }, 2));
+		System.out.println(T.solution(new String[] { "bob tom", "bob park", "park bob", "luis park", "daniel luis",
+				"luis bob", "park luis", "tom bob", "tom luis", "john park", "park john" }, 3));
 	}
 }
